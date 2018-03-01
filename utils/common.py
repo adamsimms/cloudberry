2027c5b28911f0b063c4952c1612b8c422bf27ea
RPi Guru <rpiguru@techie.com>
2018-03-01 18:23:22 +1000

    Parsed the whole html page to get the created datetime of images/videos.

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import xmltodict
+import datetime
 
 
 def count_down(s):
@@ -26,3 +28,26 @@ def get_serial():
         cpuserial = "ERROR000000000"
 
     return cpuserial
+
+
+def parse_media_page(content):
+    data = xmltodict.parse(content)
+    tr_list = data['html']['body']['div']['div']['table']['tbody']['tr']
+    if type(tr_list) != list:
+        tr_list = [tr_list, ]
+    images = []
+    for tr in tr_list:
+        v = {}
+        for td in tr['td']:
+            if 'a' in td:
+                v['name'] = td['a']['#text']
+            elif 'span' in td and type(td['span']) != list:
+                v['date'] = datetime.datetime.strptime(td['span']['#text'], "%d-%b-%Y %H:%M")
+        if '.JPG' in v['name'] or '.MP4' in v['name']:
+            images.append(v)
+    return sorted(images, key=lambda img: img['name'])
+
+
+if __name__ == '__main__':
+    _content = open('sample_response.xml').read()
+    print(parse_media_page(_content))

20c98820afa951346c5d25584b8172fc9c5b8f1b
RPi Guru <rpiguru@techie.com>
2020-11-12 11:25:20 +0800

    Working on #3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 cur_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 user="$(id -u -n)"
 
+sudo apt update
+
+sudo apt install python-dev python-pip
+
+sudo pip install -r picamera boto wakeonlan
+
+
 sudo sed -i -- "s/^exit 0/su ${user} -c \'screen -mS c -d\'\\nexit 0/g" /etc/rc.local
 sudo sed -i -- "s/^exit 0/su ${user} -c \'screen -S c -X stuff \"cd ${cur_dir////\\/}\\\\r\"\'\\nexit 0/g" /etc/rc.local
 sudo sed -i -- "s/^exit 0/su ${user} -c \'screen -S c -X stuff \"python main.py\\\\r\"\'\\nexit 0/g" /etc/rc.local
